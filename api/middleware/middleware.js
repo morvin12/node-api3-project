41ee5aeeb49206c65d08c85146a4a268c0214d90
Michael Orvin <morvin1212@gmail.com>
2022-01-12 17:36:40 -0700

    Middleware is complete, now working on the endpoints

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -29,7 +29,6 @@ async function validateUserId(req, res, next) {
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
-
   const { name } = req.body
   if(!name || !name.trim()) {
     res.status(400).json({
@@ -43,8 +42,15 @@ function validateUser(req, res, next) {
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-console.log('validatePost middleware')
-next()
+  const { text } = req.body
+  if(!text || !text.trim()) {
+    res.status(400).json({
+      message: "missing required text field"
+    })
+  } else {
+    req.text = text.trim()
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
